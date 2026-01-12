0f6d9ba3988c02b75e81c6f00bef24ca1153a58b
lirongmusic <lironglin1224@gmail.com>
2026-01-12 16:42:04 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -16,16 +16,16 @@ CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
 # --- 設定區 ---
 # 請將這裡換成你的 Google 試算表 CSV 網址
 # 記得網址最後面應該要是 output=csv
-SHEET_URL = '請把你的_Google_Sheet_CSV_網址貼在這裡'
+SHEET_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQdYAgLzQ-n30rFghdLst7K3GIlp34QP8trjUtTIBTCV9dLEuDLH3ZEP6dBYWXw-K4LScsv0WFy9duF/pub?output=csv'
 
-line_bot_api = LineBotApi(Y2mnKuvayM6qdO/W89+BZQ0/oeIgHG5mkfbwhTSwom0FbyU44+dU/KztzSTKPoZq1JecGcEPgyogEaFkvJ1oBvFsrr576RcXI0FY0Jlklae8+/z6GHYTfUziEKWAch9KBKKJGVEJcNVbrI9pSjbMBQdB04t89/1O/w1cDnyilFU=)
-handler = WebhookHandler(908eea58ffed664f86dd4bc1fb9f8e30)
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
 
 # --- 讀取 Google Sheet 的函式 ---
 def get_reply_from_sheet(user_text):
     try:
         # 下載 CSV 檔案
-        response = requests.get(https://docs.google.com/spreadsheets/d/e/2PACX-1vQdYAgLzQ-n30rFghdLst7K3GIlp34QP8trjUtTIBTCV9dLEuDLH3ZEP6dBYWXw-K4LScsv0WFy9duF/pub?output=csv)
+        response = requests.get(SHEET_URL)
         response.encoding = 'utf-8' # 設定編碼以免中文亂碼
         
         # 解析 CSV
